371ca7a9d774f0c62114a7d801fd3da570567cb1
Ankur Dalal <ankur.dalal@aptiv.com>
2023-11-09 21:45:17 -0500

    missing ; compile issue

--- roman2string/src/roman2string.cpp
+++ roman2string/src/roman2string.cpp
@@ -2,34 +2,33 @@
 #include <string>
 #include <unordered_map>
 
-roman2string::roman2string(const int &num) : num_(num) 
+roman2string::roman2string(const int &num) : num_(num)
 {
 
-    unordered_map<int,string> r2s_map = {
-        {1,"I"},
-        {2,"II"},
-        {3,"III"},
-        {4,"IV"},
-    }
+    unordered_map<int, string> r2s_map = {
+        {1, "I"},
+        {2, "II"},
+        {3, "III"},
+        {4, "IV"},
+    };
     // extract the digits
     uint8_t factor = 10;
     uint8_t n = 0;
     string out = "";
 
-    while(int(num/pow(factor,n)) > 0) {
+    while (int(num / pow(factor, n)) > 0)
+    {
 
-        int temp = num%(int(pow(factor,n+1)));
-        cout<<std::to_string(temp)<<endl;
+        int temp = num % (int(pow(factor, n + 1)));
+        cout << std::to_string(temp) << endl;
 
         // compare the temp in ranges
         string t_str = "";
-        
 
         // add the roman to string
         out = t_str + out;
         n++;
     }
-
 }
 
-roman2string::~roman2string(){}
+roman2string::~roman2string() {}
